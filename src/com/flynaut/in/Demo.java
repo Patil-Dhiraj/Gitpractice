ae58023115dd5f21ba247224853a37b10581d83a
Patil-Dhiraj <dhirajpatil7107@gmail.com>
2024-05-15 14:52:06 +0530

    first commit

---
+++ src/com/flynaut/in/Demo.java
@@ -0,0 +1,15 @@
+package com.flynaut.in;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10;
+		int b=20;
+		int c= a+b;
+		
+		System.out.println("hello Github...");
+		System.out.println("Addition = "+c);
+	}
+
+}
